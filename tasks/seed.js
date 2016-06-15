0133392633d0f8b9dcf9e5776c01d941f3a8459b
melissajimison <melissajimison@gmail.com>
2016-06-15 15:46:34 -0700

    seeded the Rentals table

--- tasks/seed.js
+++ tasks/seed.js
@@ -4,12 +4,16 @@ var db = massive.connectSync({connectionString : connectionString});
 var data_movies = require('../db/seeds/movies.json');
 var data_customers = require('../db/seeds/customers.json');
 
-
 for (var record of data_movies) {
   db.movies.saveSync(record)
-}
+
+  for (i = 0; i < record.inventory; i++) {
+    db.rentals.saveSync({movie_id: record.id })
+  };
+};
 
 for (var record of data_customers) {
   db.customers.saveSync(record)
-}
+};
+
 process.exit( );
