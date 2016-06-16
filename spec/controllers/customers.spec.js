7319eb2f4dc6d38504eeae61de4be29b42deb780
Risha <rishallen@gmail.com>
2016-06-16 15:34:52 -0700

    created test

---
+++ spec/controllers/customers.spec.js
@@ -0,0 +1,15 @@
+var request = require('request');
+var base_url = "http://localhost:3000";
+
+describe("Endpoint at /", function () {
+  var url = function(endpoint) {
+    return base_url + "customers" + endpoint;
+  };
+
+  it('responds with a 200 status code', function (done) {
+    request.get(url('/'), function(error, response, body) {
+      expect(response.statusCode).toEqual(200);
+      done();
+    });
+  });
+});
