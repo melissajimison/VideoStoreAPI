fc9706b11d77ed5647c05ae09ce10ac2e17a7cd8
Lisa Rolczynski <lisa.sano@gmail.com>
2016-06-15 17:30:14 -0700

    fixed a bug that was not capturing the id of the movie to populate into the rentals table when seeding.

--- tasks/seed.js
+++ tasks/seed.js
@@ -5,11 +5,14 @@ var data_movies = require('../db/seeds/movies.json');
 var data_customers = require('../db/seeds/customers.json');
 
 for (var record of data_movies) {
-  db.movies.saveSync(record)
-
-  for (i = 0; i < record.inventory; i++) {
-    db.rentals.saveSync({movie_id: record.id })
-  };
+  db.movies.saveSync(record);
+  db.movies.find({title: record.title}, function(err, res){
+    for (i = 0; i < record.inventory; i++) {
+      db.rentals.saveSync({movie_id: res[0].id })
+      // console.log(record.title + i);
+      console.log(res[0].id);
+    };
+  });
 };
 
 for (var record of data_customers) {
