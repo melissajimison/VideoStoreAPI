4d837aca7b23a0498cf3de58de1019f4940eac18
Risha <rishallen@gmail.com>
2016-06-17 10:50:09 -0700

    added the flow to send requests and retrieve json response.

--- controllers/customers_controller.js
+++ controllers/customers_controller.js
@@ -13,7 +13,30 @@ var CustomersController = {
         // res.render("accounts/index",locals);
         }
     });
-  }
-};
+  },
 
+sort: function(req, res, next) {
+  // // GET /search?q=tobi+ferret
+  // req.query.q
+  // // => "tobi ferret"
+
+  //Send in an ORDER clause and a LIMIT with OFFSET
+  var options = {
+    limit : req.query.n,
+    order : req.params.column,
+    offset: req.query.p
+  };
+
+  // products ordered in descending fashion
+  Customers.sort_by(options, function(error, customers) { // this is a callback
+    if(error) {
+      var err = new Error("No such customer");
+      err.status = 404;
+      next(err);
+    } else {
+      res.json(customers);
+    }
+  });
+}
+};
 module.exports = CustomersController;
