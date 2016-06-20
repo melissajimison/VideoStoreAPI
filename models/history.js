5eb45ae23be0d7927b6d3b446a018936d4b463ee
Risha <rishallen@gmail.com>
2016-06-20 12:52:03 -0700

    added the history function

---
+++ models/history.js
@@ -0,0 +1,24 @@
+var app = require("../app");
+var db = app.get("db");
+
+// Constructor function
+var History = function(history) {
+  this.id = history.id;
+  this.rental_id = history.rental_id;
+  this.customer_id = history.customer_id;
+  this.checkout_date = history.checkout_date;
+  this.return_date = history.return_date;
+
+};
+
+History.find_by_customer_id = function(ids, callback) {
+  db.customers.find({id: ids}, function(error, customers) {
+    if(error || !customers) {
+      callback(error || new Error("Could not retrieve information"), undefined);
+    } else {
+      callback(null, customers.map(function(customer) {
+        return new History(customer);
+      }));
+    }
+  });
+};
