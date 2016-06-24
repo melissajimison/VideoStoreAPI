2881e5c9fab3178f917a79109848d548a8c6e7a6
Risha <rishallen@gmail.com>
2016-06-24 09:30:18 -0700

    crerating json api flow.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var IndexController = require('../controllers/index_controller.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,5 +10,11 @@ router.get('/', function(req, res, next) {
 router.get('/zomg', function(req, res, next) {
   res.status(200).json({it_works: 'it works!'});
 });
+
+// router.get('/api/docs.json', function (req, res, next){
+//   res.status(200).json(docs);
+// });
+
+router.get('/api/docs.json', IndexController.getjson);
 
 module.exports = router;
--- spec/controllers/customers.spec.js
+++ spec/controllers/customers.spec.js
@@ -6,6 +6,24 @@ describe("Endpoint at /", function () {
     return base_url + "customers" + endpoint;
   };
 
+  it('responds with a 200 status code', function (done) {
+    request.get(url('/'), function(error, response, body) {
+      expect(response.statusCode).toEqual(200);
+      done();
+    });
+  });
+});
+
+//
+// describe("the returned json data", function() {
+//   it('has the right keys', function(done) {
+//     request.get(base_url, function(error, response, body) {
+//       var data = JSON.parse(body);
+//       expect(Object.keys(data)).toEqual(['']);
+//       done();
+//     });
+//   });
+
   // it('responds with a 200 status code', function (done) {
   //   request.get(url('/'), function(error, response, body) {
   //     expect(response.statusCode).toEqual(200);
